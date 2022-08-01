b20c4f8d57fe14982c4e93bd6bdba9837679fc9c
kleintrevor <trevor.klein@gmail.com>
2022-07-31 21:20:42 -0500

    initial upload of files in project

---
+++ script.js
@@ -0,0 +1,42 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+const CANVAS_WIDTH = canvas.width = 600;
+const CANVAS_HEIGHT = canvas.height = 600;
+
+const playerImage = new Image();
+playerImage.src = 'shadow_dog.png';
+const spriteWidth = 575;
+const spriteHeight = 523;
+let frameX = 0;
+let frameY = 0;
+let gameFrame = 0;
+const staggerFrames = 5;
+const spriteAnimations = [];
+const animationStates = [
+  {
+    name: 'idle',
+    frames: 7,
+  }
+  {
+    name: 'jump',
+    frames: 7,
+  }
+];
+
+
+
+function animate(){
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    let position = Math.floor(gameFrame/staggerFrames) % 6;
+    frameX = spriteWidth * position;  //advanced calculation, pulled from internet
+    ctx.drawImage(playerImage, frameX, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+   // if (gameFrame % staggerFrames == 0){
+   //    if (frameX < 6) frameX++;
+   //    else frameX = 0;
+   //}
+
+    gameFrame++;
+    requestAnimationFrame(animate);
+
+};
+animate();
