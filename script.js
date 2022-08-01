29987d50c827a79876c22c2310ab0e0c66f41406
kleintrevor <trevor.klein@gmail.com>
2022-07-31 21:55:20 -0500

    adding more code, refactoring the old ani process

--- script.js
+++ script.js
@@ -7,8 +7,6 @@ const playerImage = new Image();
 playerImage.src = 'shadow_dog.png';
 const spriteWidth = 575;
 const spriteHeight = 523;
-let frameX = 0;
-let frameY = 0;
 let gameFrame = 0;
 const staggerFrames = 5;
 const spriteAnimations = [];
@@ -23,12 +21,23 @@ const animationStates = [
   }
 ];
 
-
+animationStates.forEach((state, index) => {
+  let frames = {
+    loc: [],
+  }
+  for (let j = 0; j < state.frames; j++){
+    let positionX = j * spriteWidth;
+    let positionY = index * spriteHeight;
+    frames.loc.push({x: positionX, y: positionY});
+  }
+  spriteAnimations[state.name] = frames;
+});
 
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    let position = Math.floor(gameFrame/staggerFrames) % 6;
-    frameX = spriteWidth * position;  //advanced calculation, pulled from internet
+    let position = Math.floor(gameFrame/staggerFrames) % spriteAnimations['idle'].loc.length;
+    let frameX = spriteWidth * position;  //advanced calculation, pulled from internet
+    let frameY = spriteAnimations['idle'].loc[position].y;
     ctx.drawImage(playerImage, frameX, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
    // if (gameFrame % staggerFrames == 0){
    //    if (frameX < 6) frameX++;
